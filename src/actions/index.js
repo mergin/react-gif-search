8a9e0090561bd42ef509a17ee7c2950e4d29a152
mergin <merginvalhala@gmail.com>
2017-08-22 15:45:20 -0400

    Finished second part of the tutorial.

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import request from 'superagent';
+
+// action type
+export const REQUEST_GIFS = 'REQUEST_GIFS';
+
+const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const API_KEY = '&api_key=dc6zaTOxFJmzC';
+
+// action creator
+export function requestGifs(term = null) {
+    const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
+
+    // action
+    return {
+        type: REQUEST_GIFS,
+        payload: data
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,62 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GifList from './components/GifList';
-import GifModal from './components/GifModal';
-import SearchBar from './components/SearchBar';
-import request from 'superagent';
-import './styles/app.css';
-
-class App extends React.Component {
-    constructor() {
-        
-        super();
-
-        this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalIsOpen: false
-        }
-
-        //Needed for binding 'this' if handleTermChange is NOT an arrow function
-        //this.handleTermChange = this.handleTermChange.bind(this);
-    }
-
-    openModal(gif) {
-        this.setState({
-            selectedGif: gif,
-            modalIsOpen: true
-        });
-    }
-
-    closeModal() {
-        this.setState({
-            selectedGif: null,
-            modalIsOpen: false
-        });   
-    }
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 
-    //It inherits the lexical this of its parent scope so we don't run into issues with binding.
-    handleTermChange = (term) => {
-        const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+const store = configureStore();
 
-        request.get(url, (err, res) => {
-            this.setState({ gifs: res.body.data });
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <SearchBar onTermChange={this.handleTermChange}/>
-                <GifList  gifs={this.state.gifs}
-                          onGifSelect={selectedGif => this.openModal(selectedGif)} />
-                <GifModal modalIsOpen={this.state.modalIsOpen}
-                          selectedGif={this.state.selectedGif}
-                          onRequestClose={ () => this.closeModal() } />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('app')
+);
